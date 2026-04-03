161dfdfadaf7f0d13364d50d8338f45d514906ba
Valentyna <moonight82@gmail.com>
2026-04-03 10:33:29 +0300

    Init 2

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,14 @@
-{
-  "name": "aqa_2",
-  "version": "1.0.0",
-  "type": "module",
+import js from '@eslint/js';
+import globals from 'globals';
 
-  "scripts": {
-    "lint": "eslint .",
-    "format": "prettier . --write"
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node,
+    },
   },
-
-  "devDependencies": {
-    "@eslint/js": "^10.0.1",
-    "eslint": "^10.0.3",
-    "globals": "^17.4.0",
-    "husky": "^9.1.7",
-    "lint-staged": "^16.4.0",
-    "prettier": "^3.8.1"
-  },
-
-  "lint-staged": {
-    "**/*.js": [
-      "eslint --fix",
-      "prettier --write"
-    ]
-  }
-}
+];
